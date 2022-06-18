7e3fc12de034e40c468a4a99bbfcbb947badd957
Tymophiyy <tima.matviiv@gmail.com>
2022-06-18 10:43:22 +0300

    make it better

--- try.py
+++ try.py
@@ -1,15 +1,87 @@
-import socket, threading, time, sys, signal, os
+import pyautogui
+import keyboard
 
-
-def signal_handling(signum, frame):
-    print("allright")
-
-signal.signal(signal.SIGINT,signal_handling)
-
+stopKey = "s" #The stopKey is the button to press to stop. you can also do a shortcut like ctrl+s
+maxX, maxY = pyautogui.size() #get max size of screen
 while True:
-    pass
+	if keyboard.is_pressed(stopKey):
+		break
+	else:
+		pyautogui.moveTo(maxX/2, maxY/2) #move the mouse to the center of the screen
 
-print("hello")
+
+pyautogui.FAILSAFE = False
+
+pyautogui.press(stopKey)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import keyboard
+# # from pynput.mouse import Controller
+# from time import sleep
+
+# def blockinput():
+#     global block_input_flag
+#     block_input_flag = 1
+#     t1 = threading.Thread(target=blockinput_start)
+#     t1.start()
+#     print("[SUCCESS] Input blocked!")
+	
+
+# def unblockinput():
+#     blockinput_stop()
+#     print("[SUCCESS] Input unblocked!")
+	
+
+# def blockinput_start():
+#     mouse = Controller()
+#     global block_input_flag
+#     for i in range(150):
+#         keyboard.block_key(i)
+#     while block_input_flag == 1:
+#         mouse.position = (0, 0)
+
+# def blockinput_stop():
+#     global block_input_flag
+#     for i in range(150):
+#         keyboard.unblock_key(i)
+#     block_input_flag = 0
+
+
+# blockinput()
+# print("now blocking")
+# sleep(5)
+# print("now unblocking")
+
+# keyboard.block_key(149)
+
+
+
+
+
+
+# import socket, threading, time, sys, signal, os
+
+
+# def signal_handling(signum, frame):
+#     print("allright")
+
+# signal.signal(signal.SIGINT,signal_handling)
+
+# while True:
+#     pass
+
+# print("hello")
 
 
 # try:
